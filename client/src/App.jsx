90b7e3ede22dd3faac71cf27c4d98bc9350ae2fa
Naod <naodk36@gmail.com>
2025-09-27 18:32:10 +0300

    ....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
